408d115420cddc7ac1979e5c729a91ec6cd0fe95
danielfts <d.trivino@uniandes.edu.co>
2023-11-06 05:39:02 +0000

    fixed bugs and completed generate mehods

--- src/main/java/edu/uniandes/hotelandes/hotel/room/hotelService/product/ProductGenerator.java
+++ src/main/java/edu/uniandes/hotelandes/hotel/room/hotelService/product/ProductGenerator.java
@@ -4,9 +4,11 @@ import net.datafaker.Faker;
 public class ProductGenerator {
 
      static public Product generateProduct(Faker faker) {
-     Long id = (long) faker.number().numberBetween(1, 100);
-     String name = faker.lorem().word();
-     Double cost = faker.number().randomDouble(2, 0, 1000000);
-     return new Product(id, name, cost);
+     Integer id = (Integer) faker.number().numberBetween(1, 100);
+     String name = faker.beer().name();
+     String description = faker.lorem().word();
+     Float cost = (float) faker.number().randomDouble(2, 0, 1000000);
+     Byte serviceID = (byte)faker.number().randomNumber(10, false);
+     return new Product(id, name, description, cost, serviceID);
       }
 }
--- src/main/java/edu/uniandes/hotelandes/user/role/RoleGenerator.java
+++ src/main/java/edu/uniandes/hotelandes/user/role/RoleGenerator.java
@@ -1,11 +1,29 @@
+package edu.uniandes.hotelandes.user.role;
+
 import net.datafaker.Faker;
-import Role;
 
-public class RoleGenerator{
+import java.util.ArrayList;
 
-    static public generateRole(Faker faker){
-        String roleName = faker.name().firstName();
-        String roleDesc = faker.Lorem();
-        Role role = Role(1, roleName, roleDesc);
+public class RoleGenerator {
+
+    static public ArrayList<Role> generateRoles(Faker faker) {
+        ArrayList<Role> roles = new ArrayList<Role>();
+        String roleDesc = faker.lorem().paragraph();
+        Role role1 = new Role((Byte) (byte) 1, Roles.CLIENT.name(), roleDesc);
+        roles.add(role1);
+        roleDesc = faker.lorem().paragraph();
+        Role role2 = new Role((Byte) (byte) 1, Roles.ADMINISTRATOR.name(), roleDesc);
+        roles.add(role2);
+        roleDesc = faker.lorem().paragraph();
+        Role role3= new Role((Byte) (byte) 1, Roles.EMPLOYEE.name(), roleDesc);
+        roles.add(role3);
+        roleDesc = faker.lorem().paragraph();
+        Role role4 = new Role((Byte) (byte) 1, Roles.MANAGER.name(), roleDesc);
+        roles.add(role4);
+        roleDesc = faker.lorem().paragraph();
+        Role role5 = new Role((Byte) (byte) 1, Roles.RECEPTIONIST.name(), roleDesc);
+        roles.add(role5);
+
+        return roles;
     }
 }
