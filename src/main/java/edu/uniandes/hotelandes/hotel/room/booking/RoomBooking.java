22ed9e0f98120612c3e63bb8b4c914ec8c6415ec
danielfts <d.trivino@uniandes.edu.co>
2023-11-07 23:52:39 +0000

    fixed id

--- src/main/java/edu/uniandes/hotelandes/hotel/room/booking/RoomBooking.java
+++ src/main/java/edu/uniandes/hotelandes/hotel/room/booking/RoomBooking.java
@@ -3,7 +3,7 @@ package edu.uniandes.hotelandes.hotel.room.booking;
 import java.util.Date;
 
 public record RoomBooking(
-    String id,
+    Integer id,
     Integer clientId,
     Integer hotelRoomId,
     Date checkIn,
